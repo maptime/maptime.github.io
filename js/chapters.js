e5f19ff04bb3b00d29ccff6c726b075577c5882d
Roy Hewitt <royhewitt07@gmail.com>
2014-09-24 08:21:38 -0400

    Map uses geojson from _data

--- js/chapters.js
+++ js/chapters.js
@@ -10,7 +10,7 @@ $(function() {
       scrollWheelZoom: false,
       zoomControl: false,
       worldCopyJump: true
-    });
+    }).setView([39.774769485295465,-84.55078125], 3);
 
     // swap out layers and token periodically
     var myLayer = L.tileLayer('http://api.tiles.mapbox.com/v4/grafa.88128171/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ3JhZmEiLCJhIjoiU2U2QnIzUSJ9.4LnG05Ptvi1sUQ8t68rfgw')
@@ -20,33 +20,26 @@ $(function() {
       .setPosition('bottomleft')
       .addTo(map);
 
-    var featureLayer = L.mapbox.featureLayer();
-
-    map.setView([39.774769485295465,-84.55078125], 3);
-    featureLayer.loadURL('./map/maptimes.geojson');
-
-    featureLayer.on('ready', function(e) {
-        // The clusterGroup gets each marker in the group added to it
-        // once loaded, and then is added to the map
-        
-        var clusterGroup = new L.MarkerClusterGroup({
-          showCoverageOnHover: false, 
-          maxClusterRadius: 30
-        });
-        
-        e.target.eachLayer(function(marker) {
-            clusterGroup.addLayer(marker);
-            var popupContent = '<h2>'+ marker.feature.properties.title +'</h2>' +
-              'Twitter: ' + '<a href="http://twitter.com/' + marker.feature.properties.twitter + '" target="_blank">@' + marker.feature.properties.twitter +'</a>';
-            // and bind it to the marker
-            marker.bindPopup(popupContent);
-            marker.setIcon(new L.Icon({
-              iconUrl:'./map/maptime-marker.png',
-              iconSize: [24,24],
-              iconAnchor: [6,12],
-              popupAnchor: [5,-10]
-            }));
-        });
-        map.addLayer(clusterGroup);
+    var chapters = L.geoJson( {{ site.data.chapters | jsonify }}, {
+      onEachFeature: function (feature, layer) {
+        var popupContent = '<h2>'+ feature.properties.title +'</h2>' + 'Twitter: ' + 
+          '<a href="http://twitter.com/' + feature.properties.twitter + '" target="_blank">@' + feature.properties.twitter +'</a>';
+        layer.bindPopup(popupContent);
+        layer.setIcon(new L.Icon({
+          iconUrl:'./map/maptime-marker.png',
+          iconSize: [24,24],
+          iconAnchor: [6,12],
+          popupAnchor: [5,-10]
+        }));
+      }
     });
+
+    var clusterGroup = new L.MarkerClusterGroup({
+      showCoverageOnHover: false, 
+      maxClusterRadius: 30
+    });
+
+    clusterGroup.addLayer(chapters);
+    
+    map.addLayer(clusterGroup);
 });
