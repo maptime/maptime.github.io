5bb750ec623d24a72a55da47a9f82b9f812f67d3
Roy Hewitt <royhewitt@gmail.com>
2015-05-16 11:38:54 -0400

    Moved menu logic into site.js

--- site.js
+++ site.js
@@ -26,4 +26,29 @@ function githubWatchers() {
     });
 };
 
-$(githubWatchers);
+$(function() {
+    // Menu logic
+    var nav = $('#nav'),
+        menu = $('#nav .menu'),
+        navButton = $('.nav-expand');
+    $('button.nav-expand').on('click', function(){
+      menu.slideToggle(200);
+      navButton.toggleClass('open');
+    });
+    var blocked = false,
+        threshold = 800;
+    $(window).resize(function(){
+      menuCheck($(window).outerWidth(true), blocked, threshold);
+    });
+    function menuCheck(w, b, t) {
+      if (w>t && !b) {
+        menu.show();
+        blocked = !b;
+        $('.nav-expand').removeClass('open');
+      } else if (w<=t && b) {
+        menu.hide();
+        blocked = !b;
+      }
+    }
+    githubWatchers()
+});
